7edde16cd1e441d52f7974c71a2cf3d6eb4912a7
Casper Bastiaan-Net <casper.bastiaan.net@gmail.com>
2022-04-28 13:48:47 +0200

    fixed virtual in positie.h

--- SWADP_HHS/Positie.h
+++ SWADP_HHS/Positie.h
@@ -1,5 +1,19 @@
-#pragma once
+#ifndef POSITIE_H
+#define POSITIE_H
+
+
+
 class Positie
 {
+public:
+	Positie(int, int);
+	int x();
+	int y();
+	virtual void move(int, int)=0;
+private:
+	int X;
+	int Y;
 };
+
+#endif
 
---
+++ SWADP_HHS/Robot.cpp
@@ -0,0 +1,12 @@
+#include "Robot.h"
+#include <iostream>
+
+Robot::Robot(Positie p) : pos(p) {}
+
+void Robot::run() {
+	//do stuff
+}
+
+void Robot::show() {
+	std::cout << "show stuff" << std::endl;
+}
