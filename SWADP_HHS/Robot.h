0d988dce1e04cc1451ed0806e4ef181241774042
Casper Bastiaan-Net <casper.bastiaan.net@gmail.com>
2022-04-28 13:40:24 +0200

    made class files

---
+++ SWADP_HHS/Robot.h
@@ -0,0 +1,16 @@
+#ifndef ROBOT_H
+#define ROBOT_H
+
+#include "Positie.h"
+
+class Robot
+{
+public:
+	Robot(Positie);
+	virtual void run();
+	virtual void show();
+private:
+	Positie pos;
+};
+
+#endif
